314b89aef1f3838fdb3f331313ffb0baecfee8d1
Leonardo Mello <leonardomello2100@gmail.com>
2022-02-14 10:16:53 -0300

    Initial Commit

---
+++ spotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spotify
+//
+//  Created by Leonardo Mello on 14/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
